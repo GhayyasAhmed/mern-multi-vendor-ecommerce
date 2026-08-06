74be260d280508a20be6c88306087ace7ca9c1db
GhayyasAhmed <ghayyas1ahmed@gmail.com>
2026-08-06 12:16:49 +0500

    fix(client): scope middleware to auth routes only to restore client-side navigation

--- client/middleware.ts
+++ client/middleware.ts
@@ -39,5 +39,6 @@ export function middleware(request: NextRequest) {
 }
 
 export const config = {
-  matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
+  // matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
+  matcher:  ["/login", "/signup", "/forgot-password"],
 };
